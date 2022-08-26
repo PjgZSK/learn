dde55ef43fd8f8300ca793c74487851d6f53e4df
PjgZSK <pjg2016@gmail.com>
2022-08-26 17:50:53 +0800

    stage

---
+++ cmake/tutorial/step1/TutorialConfig.h
@@ -0,0 +1,10 @@
+/*************************************************************************
+    > File Name: TutorialConfig.h
+    > Author: pjg
+    > Mail: 
+    > Created Time: 2022/8/26 9:33:25
+ ************************************************************************/
+
+// the configured options and settings for Tutorial
+#define Tutorial_VERSION_MAJOR @Tutorial_VERSION_MAJOR@
+#define Tutorial_VERSION_MINOR @Tutorial_VERSION_MINOR@
--- cmake/tutorial/step1/tutorial.cpp
+++ cmake/tutorial/step1/tutorial.cpp
@@ -8,15 +8,18 @@
 #include <cstdlib>
 #include <iostream>
 #include <cmath>
+#include "TutorialConfig.h"
 
 int main (int argc, char** argv)
 {
     if (argc < 2) 
     {
+        //report version
+        std::cout << argv[0] << " Version " << Tutorial_VERSION_MAJOR << "." << Tutorial_VERSION_MINOR << std::endl;
         std::cout << "please input a number !\n";
-        return 0;
+        return 1;
     }
-    int num = std::atoi(argv[1]);
+    double num = std::atof(argv[1]);
     std::cout << "the square root of " << num << " is " << std::sqrt(num) << "\n";
     return 0;
 }
