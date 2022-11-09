4b58c234ca4ecd0262e94417d30e7b56087e9b54
PjgZSK <pjg2016@gmail.com>
2022-11-09 11:59:56 +0800

    pythagorean

--- math/main.cpp
+++ math/main.cpp
@@ -8,9 +8,11 @@
 #include <iostream>
 #include <stdio.h>
 #include <vector>
+#include <algorithm>
 #include "squareRoot.h"
+#include "pythagorean.h"
 
-int main(int argc, char * argv[])
+void testSquareRoot()
 {
     std::vector<float> fvec{ 0.999, 2.3345, 0.234 , 345 , 4 , 64 , 1889, 565656 };
     for (auto f : fvec)
@@ -20,5 +22,36 @@ int main(int argc, char * argv[])
         auto fi = squareRoot(f, SRImplemention::eFastInverse);
         printf("value %f 's square root is :\n %f [midsearch]\n %f [newton]\n %f [fast inverse]\n\n", f, ms, nt, fi);
     }
+}
+
+void testPythagorean()
+{
+    using namespace pythagorean;
+    std::vector<Pitch> pitchs;
+    Pitch upPitch(Fraction(1));
+    Pitch downPitch(Fraction(1));
+    pitchs.push_back(upPitch);
+    for (int i = 0; i < 5; ++i)
+    {
+        upPitch.nextUp();
+        downPitch.nextDown();
+        //printPitch(downPitch);
+        pitchs.push_back(upPitch);
+        pitchs.push_back(downPitch);
+    }
+    //printf("\n");
+    std::sort(pitchs.begin(), pitchs.end(), [](const Pitch& l, const Pitch& r) {
+        return l.value < r.value;
+    });
+
+    for (auto& pitch : pitchs)
+    {
+        printPitch(pitch);        
+    }
+}
+
+int main(int argc, char * argv[])
+{
+    testPythagorean();
     return 0;
 }
