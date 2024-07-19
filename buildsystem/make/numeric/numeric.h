0aaa769a32fae12912e54f7e99ed09ad7e487719
pjg-win <pjg202311@gmail.com>
2024-07-19 09:52:46 +0800

    rm buildsystem

--- buildsystem/make/numeric/numeric.h
+++
@@ -1,9 +0,0 @@
-/*************************************************************************
-    > File Name: numeric.h
-    > Author: pjg
-    > Mail: 
-    > Created Time: 二  7/26 21:21:09 2022
- ************************************************************************/
-
-unsigned gcd(unsigned a, unsigned b);
-unsigned lcm(unsigned a, unsigned b);
